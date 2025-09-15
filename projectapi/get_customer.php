9aca3b93d0ee887883bf6b59f7f1431f61e3a2f4
LOKalex <d4867806@gmail.com>
2025-09-15 19:15:27 +0800

    Add files via upload

--- projectapi/get_customer.php
+++ projectapi/get_customer.php
@@ -7,7 +7,7 @@ if ($conn->connect_error) {
     die(json_encode(["error" => $conn->connect_error]));
 }
 
-$result = $conn->query("SELECT sid, sname, srole, stel FROM staff");
+$result = $conn->query("SELECT cid, cname, ctel, caddr, company FROM customer");
 $out = $result->fetch_all(MYSQLI_ASSOC);
 echo json_encode($out, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 $conn->close();
